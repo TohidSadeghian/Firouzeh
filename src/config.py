21f5f52758dbb36971993a6efb90f3b8813bbfb8
tohid <tohid.sadeghian0000@gmail.com>
2024-11-21 12:35:02 +0330

    added redis config for cache

--- src/config.py
+++ src/config.py
@@ -4,3 +4,7 @@ import os
 DATABASE_USER=os.environ.get("DATABASE_USER")
 DATABASE_PASSWORD=os.environ.get("DATABASE_PASSWORD")
 DATABASE_NAME=os.environ.get("DATABASE_NAME")
+
+# redis
+REDIS_PASSWORD=os.environ.get("REDIS_PASSWORD")
+REDIS_NAMESPACE= os.environ.get("REDIS_PASSWORD", "url_shortener")
--- src/dependencies.py
+++ src/dependencies.py
@@ -1,9 +1,17 @@
 from .url_shortener.database import SessionLocal
-
-
+from .config import REDIS_PASSWORD, REDIS_NAMESPACE
 def get_db():
 	db = SessionLocal()
 	try:
 		yield db
 	finally:
 		db.close()
+
+
+from aiocache import Cache
+
+
+def redis_cache():
+    return Cache.REDIS(endpoint="urlshortener_redis", port=6379, password=REDIS_PASSWORD,namespace=REDIS_NAMESPACE)
+
+cache = redis_cache()

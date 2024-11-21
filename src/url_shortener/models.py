d2aa36b4756f3b991d2d7f046905628e8865463b
tohid <tohid.sadeghian0000@gmail.com>
2024-11-21 11:23:54 +0330

    fixed url model

--- src/url_shortener/models.py
+++ src/url_shortener/models.py
@@ -7,6 +7,6 @@ class URLShortener(Base):
     __tablename__ = "URLShortener"
 
     id = Column(Integer, primary_key=True)
-    original_url = Column(String, unique=True, index=True)
-    shortened_url = Column(String, index=True)
+    original_url = Column(String, index=True)
+    shortened_url = Column(String, index=True, unique=True)
     created_at = Column(DateTime, default=func.now())
